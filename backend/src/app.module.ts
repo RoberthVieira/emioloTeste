afdcff812f102fccd9611d19e4a191f3aa62f427
Roberth Vieira <135480177+RoberthVieira@users.noreply.github.com>
2025-12-15 20:34:42 -0300

    chore: configurar conexão do NestJS com MongoDB Atlas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    //Lê o .env
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    //Conecta com o MongoDB
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+        if (!uri) throw new Error('MONGO_URI não definido no .env');
+
+        return { uri };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
